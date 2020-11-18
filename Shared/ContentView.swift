dba9b981e99e383eb33dbfed137b2ed250fe4fc3
h4r100 <sriharik574@gmail.com>
2020-11-18 14:46:48 +0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Srihari on 18/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: Workspace_ManagerDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(document: .constant(Workspace_ManagerDocument()))
+    }
+}
---
+++ Shared/Workspace_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workspace_ManagerApp.swift
+//  Shared
+//
+//  Created by Srihari on 18/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Workspace_ManagerApp: App {
+    var body: some Scene {
+        DocumentGroup(newDocument: Workspace_ManagerDocument()) { file in
+            ContentView(document: file.$document)
+        }
+    }
+}
